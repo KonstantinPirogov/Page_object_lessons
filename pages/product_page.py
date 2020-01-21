62919eb43909fb345dd1215947f494df9f5a85b9
kpirogov <k.pirogov@8bitgroup.com>
2020-01-21 19:21:12 +0300

    add step43_4

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,7 +8,8 @@ class ProductPage(BasePage):
         add_item = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_item.click()
         self.solve_quiz_and_get_code()
+        item_name_string = f"{item_name} has been added to your basket."
         message_text = self.browser.find_element(*ProductPageLocators.ADD_ITEM_TEXT).text
         basket_text = self.browser.find_element(*ProductPageLocators.PRICE_BASKET_ALERT).text
-        assert item_name in message_text, f"ожидаю '{item_name}'  - подстрока '{message_text}'"
+        assert item_name_string in message_text, f"ожидаю '{item_name_string}' - строка '{message_text}'"
         assert item_price in basket_text, f"ожидаю '{item_price}'  - подстрока '{basket_text}'"
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
 from pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo_offer', ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_basket()
