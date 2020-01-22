f9db8e07c214f80bbe2c0423be942a57b75128bf
kpirogov <k.pirogov@8bitgroup.com>
2020-01-22 19:19:48 +0300

    add step43_6 negative tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,12 +4,30 @@ from pages.locators import ProductPageLocators
 class ProductPage(BasePage):
     def add_item_to_basket(self):
         item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
-        item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
         add_item = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_item.click()
         self.solve_quiz_and_get_code()
         item_name_string = f"{item_name} has been added to your basket."
         message_text = self.browser.find_element(*ProductPageLocators.ADD_ITEM_TEXT).text
-        basket_text = self.browser.find_element(*ProductPageLocators.PRICE_BASKET_ALERT).text
         assert item_name_string in message_text, f"ожидаю '{item_name_string}' - строка '{message_text}'"
-        assert item_price in basket_text, f"ожидаю '{item_price}'  - подстрока '{basket_text}'"
+
+    def add_item_to_basket_without_matan(self):
+        item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        add_item = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_item.click()
+        item_name_string = f"{item_name} has been added to your basket."
+        message_text = self.browser.find_element(*ProductPageLocators.ADD_ITEM_TEXT).text
+        assert item_name_string in message_text, f"ожидаю '{item_name_string}' - строка '{message_text}'"
+
+    def check_basket_price(self):
+        item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+        basket_price_text = self.browser.find_element(*ProductPageLocators.PRICE_BASKET_ALERT).text
+        assert item_price in basket_price_text, f"ожидаю '{item_price}'  - подстрока '{basket_price_text}'"
+
+    def no_success_message_on_page(self):
+        # проверяем, что при переходе на страницу нет успешного добавления в корзину
+        assert self.is_not_element_present(*ProductPageLocators.ADD_ITEM_TEXT), "Элемент появился!!!"
+
+    def success_message_are_vanish(self):
+        # проверяем, что сообщение о добавлении исчезает
+        assert self.is_not_element_present(*ProductPageLocators.ADD_ITEM_TEXT), "Элемент не исчез!!!"
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,33 @@
 from pages.product_page import ProductPage
 import pytest
 
-@pytest.mark.parametrize('promo_offer', ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"])
+#@pytest.mark.need_review
+@pytest.mark.parametrize('promo_offer', ["0","1", "2", "3", "4", "5", "6",  pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_basket()
+    page.check_basket_price()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-age-of-the-pussyfoot_89/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket_without_matan()
+    page.no_success_message_on_page()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-age-of-the-pussyfoot_89/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.no_success_message_on_page()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-age-of-the-pussyfoot_89/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket_without_matan()
+    page.success_message_are_vanish()
